4d0f3afdc35ff6b980a16fc559a6fc9ad7fe39c8
Dan Griffin <daniel.griffin@mckesson.com>
2020-04-28 16:18:11 -0500

    Initial Commit

---
+++ CombineWatchosTimeOut WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineWatchosTimeOut WatchKit Extension
+//
+//  Created by Daniel Griffin on 4/28/20.
+//  Copyright © 2020 McKesson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CombineWatchosTimeOut WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  CombineWatchosTimeOut WatchKit Extension
+//
+//  Created by Daniel Griffin on 4/28/20.
+//  Copyright © 2020 McKesson. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
